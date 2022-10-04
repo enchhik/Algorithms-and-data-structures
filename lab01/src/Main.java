f522374a86b1d6ef1349fc926d6ba70aa3fea78f
Denys Ostrometskyi <denys.ostrometskyi@gmail.com>
2022-10-04 17:24:28 +0300

    Add methods: fill, randomAccess

--- lab01/src/Main.java
+++ lab01/src/Main.java
@@ -1,5 +1,35 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> arrayList = new ArrayList<>();
+        List<Integer> linkedList = new LinkedList<>();
     }
+
+    private static void fill(List<Integer> list, int count, String listType) {
+        long time = System.currentTimeMillis();
+
+        Random rand = new Random();
+        for (int i = 0; i < count; i++) {
+        list.set(i, rand.nextInt(count));
+        }
+
+        System.out.printf("Fill %s: %s%n", listType, System.currentTimeMillis() - time);
+    }
+
+    private static void randomAccess(List<Integer> list, int count, String listType) {
+        long time = System.currentTimeMillis();
+
+        Random rand = new Random();
+        int index = rand.nextInt(count);
+        list.get(count);
+
+        System.out.printf("Random Access %s: %s%n", listType, System.currentTimeMillis() - time);
+    }
+
+    //private static void sequantialAccess
 }
